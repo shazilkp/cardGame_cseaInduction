bead23dbe5dd1f56429b7aed851b81905292b765
Shazil Mohammmed K P <shazilmohemmedkp@gmail.com>
2024-12-21 17:36:31 +0530

    initial commit with a working demo

---
+++ src/App.jsx
@@ -0,0 +1,198 @@
+import { useEffect, useState } from "react";
+import Card from "./Card"
+import './App.css'
+import Confetti from "react-confetti";
+
+function App() {
+  const [grid,setGrid] = useState([]);
+  const [cardData,setCardData] = useState([]);
+  const [flipCard,setFlipCard] = useState(false);
+  const [foundArray,setFoundArray] = useState([]);
+  const [isClickable,setIsClickable] = useState(true);
+  const [matches,setMatches] = useState(0);
+  const [moveCount,setMoveCount] = useState(0);
+  const [time,setTime] = useState(0);
+  const [gridSize,setGridSize] = useState(4);
+  const [gameOver,setGameOver] = useState(false);
+  const [bestScore, setBestScore] = useState(0);
+  const [resetGame,setResetGame] = useState(false);
+  const [theme,setTheme] = useState('food');
+  const [playing,setPlaying] = useState(false);
+
+
+  const foodImg= ["🍏","🍉","🍓","🥥","🫑","🥓","🫐","🍡","🍏","🍉","🍓","🥥","🫑","🥓","🫐","🍡",
+                  "🥐","🥨","🥞","🥯","🍞","🍖","🍔","🍟","🍕","🧆","🥐","🥨","🥞","🥯","🍞","🍖","🍔","🍟","🍕","🧆",
+                  "🥙","🥘","🥗","🍿","🍠","🍱","🍳","🧈","🍥","🍝","🍙","🧀","🥙","🥘","🥗","🍿","🍠","🍱","🍳","🧈","🍥","🍝","🍙","🧀","🥚","🧆","🥚","🧆"];
+
+  const animalImg = ["🐶","🐺","🐱","🐯","🐴","🐮","🐷","🐵","🐶","🐺","🐱","🐯","🐴","🐮","🐷","🐵",
+                     "🐭","🐹","🐰","🐻","🐨","🐼","🐔","🐥","🐧","🐸","🐭","🐹","🐰","🐻","🐨","🐼","🐔","🐥","🐧","🐸",
+                     "🐢","🐍","🐲","🐳","🐜","🐙","🐚","🐛","🐡","🐠","🐝","🐞","🐦","🐩","🐢","🐍","🐲","🐳","🐜","🐙","🐚","🐛","🐡","🐠","🐝","🐞","🐦","🐩"];
+  
+
+  
+  useEffect(() => {
+    // Load best score from localStorage on mount
+    const storedBestScore = localStorage.getItem(`bestscore_${gridSize}`);
+    if (storedBestScore) {
+      setBestScore(Number(storedBestScore));
+    }
+    else{
+      setBestScore(0);
+    }
+  }, [gridSize])
+  
+
+  useEffect(() => {
+    const initialGrid = [];
+    setFoundArray([]);
+    setMatches(0);
+    setMoveCount(0);
+
+    var j = 0;
+    let cardImgCopy;
+    if(theme == 'animal'){
+      cardImgCopy = [...animalImg];
+    }
+    else if(theme == 'food'){
+      cardImgCopy = [...foodImg];
+    }
+
+
+    // initialize the grid
+    for(let i = (gridSize*gridSize) -1 ;i >= 0 ; i--)  {
+        j = Math.floor(Math.random() * (i+1));
+        initialGrid.push({
+          img: cardImgCopy[j],
+          state: 'closed'
+        });
+        cardImgCopy.splice(j,1);
+    }
+    setGrid(initialGrid);
+  }, [gridSize,theme,resetGame])
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      if(!gameOver && playing){
+        setTime(time+1);
+      };
+      
+    }, 1000);
+    return () => clearInterval(int);
+  }, [time,gameOver,playing])
+
+  useEffect(() =>{
+    if(foundArray.length == gridSize*gridSize){
+      setGameOver(true);
+      setPlaying(false);
+      let score = Math.floor(100000 * (1/time) * (1/moveCount))
+      console.log(score);
+      const currentBestScore = localStorage.getItem(`bestscore_${gridSize}`);
+      if (!currentBestScore || score > Number(currentBestScore)) {
+        localStorage.setItem(`bestscore_${gridSize}`, score);
+        setBestScore(score); // Update UI
+      }
+    }
+  },[foundArray])
+
+ 
+  useEffect(() => {
+    if(resetGame){
+      setTime(0);
+      setMatches(0);
+      setFoundArray([]);
+      setMoveCount(0);
+      setPlaying(false);
+      setResetGame(false);
+      setGameOver(false);
+    }
+  },[resetGame])
+
+  function updateCardDataFromChild(dataChild){
+    setPlaying(true);
+    setMoveCount(moveCount+1);
+     console.log(cardData)
+     if(cardData.length == 0){
+       setCardData([...cardData,dataChild]);
+     }
+     else if(cardData.length == 1){
+        setIsClickable(false);
+       if(cardData[0].img == dataChild.img){
+          setMatches(matches+1);
+          setFoundArray([...foundArray, dataChild.index,cardData[0].index]);
+       }
+       else{
+          setFlipCard(true);
+          setTimeout(() => {setFlipCard(false)},1000);
+       }
+       setTimeout(() => {setIsClickable(true)},1000)
+       setCardData([]);
+     }
+   }
+
+  
+  const gridEls = grid.map((el, i) => <Card key={i} index = {i} foundArray = {foundArray} flipCard = {flipCard} setFlipCard = {setFlipCard} isClickable = {isClickable} sendDataToParent = {updateCardDataFromChild} gridSize = {gridSize} resetGame= {resetGame}>{el.img}</Card>)
+
+  
+
+   function resetGameOnClick(){
+      console.log("clicked")
+      setResetGame(true);
+   }
+
+   console.log(resetGame)
+   
+  function handleOption(themeValue){
+    if(themeValue == 'animal' && theme != 'animal'){
+      setTheme('animal');
+      setResetGame(true);
+    }
+    else if(themeValue == 'food' && theme != 'food'){
+      setTheme('food');
+      setResetGame(true);
+    }
+  }
+
+  function changeGridSize(gridSize){
+    setGridSize((gridSize+2) % 9 < 4 ? 4 : (gridSize+2) % 9);
+  }
+  return <div className="flex items-center justify-center p-2">
+  {gameOver ? <div className="fixed flex flex-col w-screen h-screen bg-slate-100 bg-opacity-65 z-10 items-center justify-center backdrop-blur-sm">
+                <Confetti width={window.innerWidth} recycle = {false} height={window.innerHeight}></Confetti>
+        
+                <h1 className="p-4" >Congratulations!!</h1>
+          
+                
+            
+                <button className="py-2 px-4 rounded bg-slate-200 font-semibold text-slate-700" onClick={() => resetGameOnClick()}>Reset Game</button>
+              </div> : ''}
+  <div className="grid justify-between items-center flex-col gap-1">
+    <div className="flex items-center flex-col justify-around md:flex-row">
+      <div className="flex justify-center items-center  flex-col">
+        <div className="flex items-center space-x-4">
+          <button className="py-2 px-4 rounded bg-slate-200 font-semibold text-slate-700" onClick={() => changeGridSize(gridSize)}>{gridSize}x{gridSize}</button>
+          <button className="py-2 px-4 rounded bg-slate-200 font-semibold text-slate-700" onClick={() => resetGameOnClick()}>Reset Game</button>
+          <select name="theme" className="py-2 px-4 rounded bg-slate-200 font-semibold text-slate-700" onChange={(e) => handleOption(e.target.value)}>
+            <option value="food">Food</option>
+            <option value="animal">Animals</option>
+          </select>
+        </div>
+        <span className="font-semibold text-slate-700 rounded py-2 px-4">Time: {Math.floor(time/60)}:{(time % 60).toString().padStart(2, '0')}</span>
+      </div>
+      <div className="flex flex-col font-semibold text-slate-700">
+        <span >Matches: {matches}</span>
+        <span>Moves: {Math.floor(moveCount/2)}</span>
+        <span>Best Score: {bestScore}</span>
+      </div>
+    </div>
+    <div className = {`grid grid-cols-${gridSize} grid-rows-${gridSize} gap-3 rounded text-white segoe-ui`} 
+    
+                      style={{width: "min(85vw,85vh)",
+                              height: "min(85vw,85vh)",}}>
+      {gridEls}
+    </div>
+  </div>
+  
+  </div>
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { useSpring, a } from '@react-spring/web'
+
+function Card({ flipCard, index, resetGame,foundArray, children, isClickable,gridSize, sendDataToParent }) {
+    const [cardState, setCardState] = useState('closed');
+
+    useEffect(() => {
+        setCardState('closed');
+    },[gridSize,resetGame])
+
+    const { transform, opacity } = useSpring({
+        opacity: cardState === 'closed' ? 1 : 0,
+        transform: `perspective(600px) rotateX(${cardState === 'closed' ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 },
+    })
+
+
+    function revealCard() {
+        if (cardState === 'closed' && isClickable) {
+            setCardState('open');
+            sendDataToParent({
+                index: index,
+                img: children,
+                state: 'open',
+            });
+        }
+    }
+
+    useEffect(() => {
+        console.log(foundArray);
+
+        foundArray.forEach((cardToChange) => {
+            if (index === cardToChange) {
+                setCardState('found');
+                console.log('hello', index)
+            }
+        });
+
+    }, [foundArray]);
+
+    useEffect(() => {
+        if (flipCard && cardState === 'open') {
+            setTimeout(() => {
+                setCardState('closed');
+            }, 1000);
+        }
+    }, [flipCard, cardState]);
+
+
+
+
+    return <div className ={'relative'} 
+       onClick={revealCard}>
+        <a.div
+        className={`absolute top-0 p-4 bg-emerald-100 border-2 border-emerald-800 rounded size-full flex justify-center items-center text-4xl`}
+        style={{ opacity: opacity.to(o => 1 - o), transform }}
+      >{children}</a.div>
+      <a.div
+        className={`absolute top-0 p-4 bg-emerald-400 border-2 border-slate-500 rounded size-full flex justify-center items-center text-4xl`}
+        style={{
+          opacity,
+          transform,
+          rotateY: '180deg',
+        }}
+      />
+    </div>
+}
+
+export default Card;
